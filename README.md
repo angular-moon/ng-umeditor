d0cf3682e9231c75169caf4cd82ea3ca74f7570a
gmsoft <admin@example.com>
2016-10-27 16:39:00 +0800

    直接返回编辑器示例而不在用ng-model绑定内容值
    因为很多操作都没有触发contentChange事件, 使用ng-model绑定值很情况不能同步内容的变化, 所以放弃了这个方案.
    需要获取内容使用editor.getContent();
    需要初始化值配置initialContent;

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 # ng-umeditor
 封装的umeditor指令
 ```javascript
-<script type="text/html" umeditor ng-model='content' content-change='changeHandler(content)' placeholder='提示文案...'></script>
+<script id="{{id}}" type="text/html" umeditor="umeditor" config="config" placeholder='提示文案...'></script>
 ```
 ***
 ### 可选的属性:
 * config: umeditor配置项
-* id: 默认使用 ng-model的变量名称作为ID, 如果需要指定id, 请使用 id={{id}}
-* content-change: 内容改变时事件处理函数
+* id: 默认使用 '' 如果需要指定id, 请使用 id='{{id}}' or id='id字面量'
 * placeholder: placeholder文字
 * drafts: 是否使用草稿自动恢复 
-  1. **注意: 启用草稿自动恢复时  placeholder 和 ng-model 的初始值将被忽略**
+  1. **注意: 启用草稿自动恢复时 placeholder 将被忽略**
   2. **当收到的事件参数id匹配时,清除草稿和内容**
   3. **用法: $scope.$broadcast('clear-umeditor', id);**
--- ng-umeditor.js
+++ ng-umeditor.js
@@ -2,13 +2,12 @@ var UM = require('umeditor');
 
 /*
 用法:
-<script type="text/html" umeditor ng-model='content' content-change='changeHandler(content)' placeholder='提示文案...'></script>
+<script type="text/html" umeditor="editor" config='config' placeholder='提示文案...'></script>
 可选的属性:
 config: umeditor配置项
-id: 默认使用 ng-model的变量名称作为ID, 如果需要指定id, 请使用 id={{id}}
-content-change: 内容改变时事件处理函数
+id: 默认使用 'umeditor', 如果需要指定id, 请使用 id={{id}}
 placeholder: placeholder文字
-drafts: 是否使用草稿自动恢复, 注意: 启用草稿自动恢复时  placeholder 和 ng-model 的初始值将被忽略
+drafts: 是否使用草稿自动恢复, 注意: 启用草稿自动恢复时 placeholder 将被忽略
         当收到的事件参数id匹配时,清除草稿和内容
         用法: $scope.$broadcast('clear-umeditor', id);
 * */
@@ -18,35 +17,25 @@ angular.module('gm.umeditor', [])
     restrict: 'AE',
     scope: {
       config: '=',
-      contentChange: '&',
+      umeditor: '=',
       id: '@',
       drafts: '='
     },
-    require: 'ngModel',
     transclude: true,
-    link: function (scope, element, attr, ngModel) {
+    link: function (scope, element, attr) {
 
       //获取当前的DOM元素
       var _dom = element[0];
 
-      //默认使用ngModel name作为编译器id, 如果需要自己指定id, 请使用id or data-id属性指定
+      //默认使用'umeditor'作为编译器id, 如果需要自己指定id, 请使用id or data-id属性指定
       //规范化id中的"."为"_"
-      var _id = (scope.id || attr.ngModel).replace(/\./g, "_");
+      var _id = (scope.id || 'umeditor').replace(/\./g, "_");
 
       _dom.setAttribute('id', _id);
 
       //是否启用草稿恢复,默认false,
       //drafts 和 placeholder冲突, 当启用drafts时会自动禁用placeholder
-      //drafts 启用时, ngModel的初始值也会被忽略
       var drafts = !!scope.drafts;
-
-      if(drafts){
-          ngModel.$setViewValue(undefined);
-      }
-
-      var _placeholder = '<p style="font-size:14px;color:#afafaf;">' +
-          (attr.placeholder || '') +
-          '</p>';
 
       var _config = scope.config || {
               toolbar: [
@@ -58,46 +47,22 @@ angular.module('gm.umeditor', [])
               initialFrameHeight: 300,
               imagePopup: true,
               autoSyncData: false,
+              enableAutoSave: drafts,
               imageUrl: '/office/imageUp.jsp'
           }
 
+      var _placeholder = drafts || _config.initialContent ? null : '<p style="font-size:14px;color:#afafaf;">' +
+                        (attr.placeholder || '') +
+                        '</p>';
 
-      var _umeditor = UM.getEditor(_id, _config);
-
-      //标记监听事件是否绑定
-      var hasListener = false;
-
-      /**
-       * 对于umeditor添加内容改变事件，内容改变触发ngModel改变.
-       */
-      var editorToModel = function () {
-          if (_umeditor.hasContents())
-              ngModel.$setViewValue(_umeditor.getContent());
-          else
-              ngModel.$setViewValue(undefined);
-
-          //调用指令绑定的contentChange事件处理
-          (scope.contentChange || angular.noop)();
-      };
+      var _umeditor = scope.umeditor = UM.getEditor(_id, _config);
 
       /**
        * umeditor准备就绪后，执行逻辑
-       * 如果ngModel有值
-       *   则给在编辑器中赋值
-       *   给编辑器添加内容改变的监听事件.
-       * 如果不存在
-       *   则写入提示文案
+       * 如果启用了草稿功能, 恢复草稿, 并忽略placeholder
+       * 如果没有启用草稿功能, 设置placeholder为编辑器的内容
        */
       _umeditor.ready(function () {
-          if (ngModel.$modelValue) {
-              _umeditor.setContent(ngModel.$modelValue);
-              _umeditor.addListener('contentChange', editorToModel);
-              hasListener = true;
-          } else {
-              if(!drafts)
-                  _umeditor.setContent(_placeholder);
-          }
-
           if(drafts){
               //恢复草稿
               _umeditor.execCommand('drafts');
@@ -110,45 +75,31 @@ angular.module('gm.umeditor', [])
                       _umeditor.execCommand('clearlocaldata');
                   }
               });
+          }else if(_placeholder){
+              _umeditor.setContent(_placeholder);
           }
-
-          ngModel.$formatters.push(function(value){
-              ngModel.$setViewValue(value);
-              _umeditor.setContent(value || '');
-              if(value && !hasListener){
-                  _umeditor.addListener('contentChange', editorToModel);
-              }
-          });
       });
+
+      function isPlaceholder(content){
+          return $(_placeholder)[0].textContent.replace(/\s/g, '') == content.replace(/\s/g, '');
+      }
 
       /**
        * 添加编辑器被选中事件
-       * 如果ngModel没有赋值
-       *   清空content
-       *   给编辑器添加内容改变的监听事件
+       * 如果内容为placehoder, 清空内容
        */
       _umeditor.addListener('focus', function () {
-          if (!ngModel.$modelValue) {
-              if(!drafts)
-                  _umeditor.setContent('');
-              _umeditor.addListener('contentChange', editorToModel);
-              hasListener = true;
-          }
+          if(!drafts && _placeholder && _umeditor.hasContents() && isPlaceholder(_umeditor.getContentTxt()))
+              _umeditor.setContent('');
       });
 
       /**
        * 添加编辑器取消选中事件
-       * 如content值为空
-       *   取消内容改变的监听事件
-       *   添加content为提示文案
+       * 如content值为空, 设置placeholder
        */
       _umeditor.addListener('blur', function () {
-          if (!ngModel.$modelValue) {
-              _umeditor.removeListener('contentChange', editorToModel);
-              hasListener = false;
-              if(!drafts)
-                  _umeditor.setContent(_placeholder);
-          }
+          if(!drafts && !_umeditor.hasContents() && _placeholder)
+              _umeditor.setContent(_placeholder);
       });
 
       //清空内容和草稿,注销编辑器
